fd805dd92e25407d113cc9d584efde506813930b
Lightrain <lightrainmail@gmail.com>
2023-07-28 10:00:28 +0800

    Initial commit

---
+++ Core/Src/freertos.c
@@ -0,0 +1,130 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for Task_main */
+osThreadId_t Task_mainHandle;
+const osThreadAttr_t Task_main_attributes = {
+  .name = "Task_main",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void Task_main_Start(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of Task_main */
+  Task_mainHandle = osThreadNew(Task_main_Start, NULL, &Task_main_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_Task_main_Start */
+/**
+  * @brief  Function implementing the Task_main thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_Task_main_Start */
+__weak void Task_main_Start(void *argument)
+{
+  /* USER CODE BEGIN Task_main_Start */
+  /* Infinite loop */
+  for(;;)
+  {
+      HAL_GPIO_TogglePin(LD2_GPIO_Port,LD2_Pin);
+    osDelay(100);
+  }
+  /* USER CODE END Task_main_Start */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
---
+++ cmake-build-debug/CMakeFiles/demo_freertos.elf.dir/DependInfo.cmake
@@ -0,0 +1,83 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Startup/startup_stm32l476rgtx.s" "C:/Users/14806/OneDrive/Desktop/demo_freertos/cmake-build-debug/CMakeFiles/demo_freertos.elf.dir/Core/Startup/startup_stm32l476rgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Inc"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Inc"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/freertos.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/freertos.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/freertos.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/gpio.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/gpio.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/main.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/stm32l4xx_hal_timebase_tim.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_hal_timebase_tim.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/stm32l4xx_it.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/syscalls.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/sysmem.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/system_stm32l4xx.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Core/Src/usart.c" "CMakeFiles/demo_freertos.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Core/Src/usart.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "C:/Users/14806/OneDrive/Desktop/demo_freertos/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/demo_freertos.elf.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
